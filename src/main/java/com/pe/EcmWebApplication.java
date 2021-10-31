38a21bd412777ef032fa687116574ed839d66f0e
nramonibarra <nilson.ramon@cordonbleu.edu.pe>
2021-10-30 23:53:44 -0500

    first commit

---
+++ src/main/java/com/pe/EcmWebApplication.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/EcmWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
